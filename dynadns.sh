883d2a14dc450f6f5c28aea3663c4eb46130b28d
Jeremy D. Impson <github@echo3.info>
2024-12-22 17:48:42 -0500

    fix help

--- dynadns.sh
+++ dynadns.sh
@@ -38,16 +38,18 @@ if test -z "$GANDI_DYNADNS_PAT"; then
 fi
 
 help() {
-	echo "Usage: $0 [ -p PAT ] <domain name> <dynamic DNS hostname> [dynamic DNS IP address]" >&2;
-	echo "  -p PAT is optional, and lets you set the GANDI_DYNADNS_PAT environment variable from the command line (not recommended)".
-	echo "	Domain name must be the domain in Gandi where you are setting the dynamic DNS address"
-	echo "	Dynamic DNS hostname must be the unqualified name you want to associate with the IP address. The resulting fully qualified domain name will be <dynamic DNS hostname>.<domain name>."
-	echo "	The optional dynamic DNS IP address is the IP address that will be associated with the hostname. If you leave off this argument, then this script will use apinfo.io to try to figure out the desired IP address. That will only work if you are on the Internet, the apinfo.io service is still running and hasn't banned you for some reason, and you are running the script on a network in such a way that ipinfo.io sees the web request as coming from the IP address you want to associate with the hostname. Personally I prefer to remote into my NAT router and grab the IP address directly rather than rely on a third party. But it's easier."
-	echo
-	echo "You must have GANDI_DYNADNS_PAT set as an environment variable, containing the PAT from your Gandi domain hosting account."
-	echo "You must have curl installed (will someday do wget)."
-	echo "You must have jq installed."
-	echo
+cat << HERE >&2;
+Usage: $0 [ -p PAT ] <domain name> <dynamic DNS hostname> [dynamic DNS IP address]
+  -p PAT is optional, and lets you set the GANDI_DYNADNS_PAT environment variable from the command line (not recommended)
+	Domain name must be the domain in Gandi where you are setting the dynamic DNS address
+	Dynamic DNS hostname must be the unqualified name you want to associate with the IP address. The resulting fully qualified domain name will be <dynamic DNS hostname>.<domain name>.
+	The optional dynamic DNS IP address is the IP address that will be associated with the hostname. If you leave off this argument, then this script will use apinfo.io to try to figure out the desired IP address. That will only work if you are on the Internet, the apinfo.io service is still running and hasn't banned you for some reason, and you are running the script on a network in such a way that ipinfo.io sees the web request as coming from the IP address you want to associate with the hostname. Personally I prefer to remote into my NAT router and grab the IP address directly rather than rely on a third party. But it's easier.
+
+You must have GANDI_DYNADNS_PAT set as an environment variable, containing the PAT from your Gandi domain hosting account.
+You must have curl installed (will someday do wget).
+You must have jq installed.
+
+HERE
 }
 
 getip() {
